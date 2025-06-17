9048bec348665bad598d467fcdd92c5a7b13cad0
Naman  Dixit <129660042+dixitnaman@users.noreply.github.com>
2025-06-17 10:06:20 +0530

    index.js

--- task manager-17-june/index.js
+++ task manager-17-june/index.js
@@ -1,76 +1,78 @@
 let id = "no";
 
-selectData();
-function manageData() {
-  document.getElementById("msg").innerHTML = "";
-  let task = document.getElementById("task").value;
-  if (task == "") {
-    document.getElementById("msg").innerHTML = "Please enter a task";
-  } else {
-    console.log(id);
-    if (id == "no") {
-      let arr = getCrudData();
-      if (arr == null) {
-        let data = [task];
-        setCrudData(data);
-      } else {
-        arr.push(task);
-        setCrudData(arr);
-      }
-      document.getElementById("msg").innerHTML = "Data added";
-    } else {
-      let arr = getCrudData();
-      arr[id] = task;
-      setCrudData(arr);
-      document.getElementById("msg").innerHTML = "Data updated";
-    }
-    document.getElementById("task").value = "";
-    selectData();
-  }
-}
-
-function selectData() {
-  let arr = getCrudData();
-  if (arr != null) {
-    let html = "";
-    let sno = 1;
-    for (let k in arr) {
-      html =
-        html +
-        `<tr>
-			<td>${sno}</td>
-			<td>${arr[k]}</td>
-			<td>
-				<a href="javascript:void(0)" onclick="editData(${k})">Edit</a>&nbsp;
-				<a href="javascript:void(0)" onclick="deleteData(${k})">Delete</a>
-			</td>
-		</tr>`;
-      sno++;
-    }
-    document.getElementById("root").innerHTML = html;
-  }
-}
-
-function editData(rid) {
-  id = rid;
-  let arr = getCrudData();
-  document.getElementById("task").value = arr[rid];
-}
-
-function deleteData(rid) {
-  if (confirm("Are you sure you want to delete this task?")) {
-    let arr = getCrudData();
-    arr.splice(rid, 1);
-    setCrudData(arr);
-    selectData();
-  }
-}
-
 function getCrudData() {
-  let arr = JSON.parse(localStorage.getItem("crud"));
-  return arr;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      let data = JSON.parse(localStorage.getItem("crud")) || [];
+      resolve(data);
+    }, 150);
+  });
 }
 
-function setCrudData(arr) {
-  localStorage.setItem("crud", JSON.stringify(arr));
+function setCrudData(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      localStorage.setItem("crud", JSON.stringify(data));
+      resolve();
+    }, 150);
+  });
 }
+
+async function manageData() {
+  document.getElementById("msg").innerHTML = "";
+  let task = document.getElementById("task").value.trim();
+
+  if (task === "") {
+    document.getElementById("msg").innerHTML = "Please enter a task";
+    return;
+  }
+
+  let arr = await getCrudData();
+
+  if (id === "no") {
+    arr.push(task);
+    document.getElementById("msg").innerHTML = "Data added";
+  } else {
+    arr[id] = task;
+    document.getElementById("msg").innerHTML = "Data updated";
+    id = "no";
+  }
+
+  await setCrudData(arr);
+  document.getElementById("task").value = "";
+  await selectData();
+}
+
+async function selectData() {
+  let arr = await getCrudData();
+  let html = "";
+  arr.forEach((task, index) => {
+    html += `
+      <tr>
+        <td>${index + 1}</td>
+        <td>${task}</td>
+        <td>
+          <a href="javascript:void(0)" onclick="editData(${index})">Edit</a>
+          <a href="javascript:void(0)" onclick="deleteData(${index})">Delete</a>
+        </td>
+      </tr>`;
+  });
+  document.getElementById("root").innerHTML = html;
+}
+
+async function editData(index) {
+  let arr = await getCrudData();
+  document.getElementById("task").value = arr[index];
+  id = index;
+}
+
+async function deleteData(index) {
+  if (confirm("Are you sure you want to delete this task?")) {
+    let arr = await getCrudData();
+    arr.splice(index, 1);
+    await setCrudData(arr);
+    await selectData();
+  }
+}
+
+selectData();
